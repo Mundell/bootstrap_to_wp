5b2b52d7a5ce22b850078aa0e43c1f80d79bef8e
Mundell <davidmundell@gmail.com>
2015-09-29 23:12:16 +0200

    all changes up to Components

--- page-portfolio.php
+++ page-portfolio.php
@@ -48,12 +48,12 @@
 
     <?php if ( have_posts() ) : while ( $the_query->have_posts() ) :$the_query->the_post(); ?>
 
+      <div class="col-xs-3 portfolio-piece">
+
       <?php
       $thumbnail_id = get_post_thumbnail_id();
       $thumbnail_url = wp_get_attachment_image_src( $thumbnail_id, 'thumbnail-size', true);
       ?>
-
-      <div class="col-xs-3 portfolio-piece">
 
       <p><a href="<?php the_permalink(); ?>"><img src="<?php echo $thumbnail_url[0]; ?>" alt="<?php the_title(); ?> graphic" /></a></p>
         <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
